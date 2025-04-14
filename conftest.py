cd65aca1ab7c76fabc82bccf1fa24ee1fbc24022
VikXUs <llokoll@yandex.ru>
2025-04-14 19:11:47 +0400

    final

--- conftest.py
+++ conftest.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
+
 webpage = 'https://stellarburgers.nomoreparties.site/'
 
 #=====================================================LOCATORS=======================================================
